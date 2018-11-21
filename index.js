ef32a6845442206d1ebd3cd52b83db13f84f09ec
Zeztron <pateltharsh@gmail.com>
2018-11-21 11:42:04 -0500

    Parsed out the url and id's for each laptop

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
 
-    const query = url.parse(req.url, true);
-    console.log(query);
-
-    res.writeHead(200, { "Content-type": "text/html"});
-    res.end("This is the response!");
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+     
+    if (pathName === "/products" || pathName === "") {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.end("This is the products page");
+    } else if (pathName === "/laptop") {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.end(`This is the Laptop page for laptop ${id}!`);
+    } else {
+        res.writeHead(404, { "Content-type": "text/html" });
+        res.end("URL was not found on the server");
+    }
 });
 
 server.listen(1337, "127.0.0.1", () => {
