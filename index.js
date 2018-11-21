a253d36bbff6bfc2d30042dddfdbe8cf0d1072fe
Zeztron <pateltharsh@gmail.com>
2018-11-21 11:32:14 -0500

    configured server and parse json data

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const fs = require('fs');
 const http = require ("http");
+const url = require("url");
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const laptopData = JSON.parse(json);
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+
+    const query = url.parse(req.url, true);
+    console.log(query);
+
+    res.writeHead(200, { "Content-type": "text/html"});
+    res.end("This is the response!");
+});
+
+server.listen(1337, "127.0.0.1", () => {
+    console.log("Server started listening");
+});
 
