c694522d05204b9093106b8f7d571dee3305e37f
Zeztron <pateltharsh@gmail.com>
2018-11-21 12:31:27 -0500

    routed to the images

--- index.js
+++ index.js
@@ -9,16 +9,21 @@ const server = http.createServer((req, res) => {
 
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
+
      
     // PRODUCTS OVERVIEW
     if (pathName === "/products" || pathName === "") {
         res.writeHead(200, { "Content-type": "text/html" });
         
         fs.readFile(`${__dirname}/templates/template-overview.html`, "utf-8", (err, data) => {
-            
-            res.end(data);
-          }
-        );
+            let overviewOutput = data;
+            fs.readFile(`${__dirname}/templates/template-card.html`, "utf-8", (err, data) => {
+
+                const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join("");
+                overviewOutput = overviewOutput.replace("{%CARDS%}", cardsOutput); 
+                res.end(overviewOutput);
+            });
+        });
 
     
     } 
@@ -32,6 +37,14 @@ const server = http.createServer((req, res) => {
         });
 
     } 
+
+    // IMAGES
+    else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img/${pathName}`, (err, data) => {
+            res.writeHead(200, { "Content-type": "image/jpg" });
+            res.end(data);
+        });
+    }
     // URL NOT FOUND
     else {
         res.writeHead(404, { "Content-type": "text/html" });
